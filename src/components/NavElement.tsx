7b1b0a58df3265c94d0de563bbec3c60988bc755
leonid riznyk <riznyk.leo@gmail.com>
2022-11-04 14:26:02 +0900

    banner and cards

--- src/components/NavElement.tsx
+++ src/components/NavElement.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-interface NavElementProps{
+interface NavElementProps {
     content: string
 }
+
 function NavElement(props: NavElementProps) {
     return (
-        <li className='text-white font-bold uppercase border-b-2 border-b-cyan-500'>
+        <li className='text-white font-bold uppercase border-b-2 border-b-cyan-300'>
             {props.content}
         </li>
     );
