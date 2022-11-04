991089a62388dc437f48b72a7e532d9646d4f054
leonid riznyk <riznyk.leo@gmail.com>
2022-11-04 16:17:18 +0900

    works and workCards

---
+++ src/components/Divider.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Divider() {
+    return (
+        <div className='flex justify-center py-8'>
+            <div className='w-[1px] h-[75px] bg-cyan-300'/>
+        </div>
+    );
+}
+
+export default Divider;
---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface HeaderProps {
+    header: string
+    prompt: string
+}
+
+function Header(props: HeaderProps) {
+    return (
+        <div className='pt-16 pb-24 mx-48'>
+            <div className='flex items-center gap-6 mb-4'>
+                <div className='w-[100px] h-[1px] bg-cyan-300 mt-2'/>
+                <h2 className='text-4xl font-bold align-middle text-white'>
+                    {props.header}
+                </h2>
+            </div>
+            <p className="text-cyan-300 uppercase">
+                {props.prompt}
+            </p>
+        </div>
+    );
+}
+
+export default Header;
